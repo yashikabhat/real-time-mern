1f75aee5933f745d428441d420e8c6777677fad6
Yashika <yabhat02@gmail.com>
2026-09-06 11:04:55 +0530

    tiny fix inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "real-time" });
@@ -27,6 +28,7 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
     async ({ event }) => {
+        await connectDB();
         const { id } = event.data;
         await User.deleteOne({ clerkId: id });
         
